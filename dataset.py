ecc615cd824ecff1772074e383b1768dc0c759d3
bredisrising <ankitmalhotra788@gmail.com>
2024-07-06 12:18:21 -0700

    is able to overtrain now, switch to conv net to scale up and find new overtrain size

--- dataset.py
+++ dataset.py
@@ -3,13 +3,99 @@ from torchvision import transforms
 from torch.utils.data import Dataset
 from PIL import Image
 
-class AudiAutoregression24px(Dataset):
+class AllVae(Dataset):
+    def __init__(self):
+        self.transform = transforms.Compose([transforms.ToTensor()])
+    
+    def __len__(self):
+        return 5190
+    
+    def __getitem__(self, index):
+        x = Image.open(f'./data/cars/all_processed_24px/{index+1}.jpg')
+        x = self.transform(x)
+        return x
+
+class AllAutoregressionGrayscale24px(Dataset):
+    def __init__(self):
+        # grayscale transform but keep 256 range
+        self.transform = transforms.Compose([
+            transforms.Grayscale(num_output_channels=1),
+            transforms.ToTensor()
+        ])
+
+
+    def __len__(self):
+        return 1628
+
+    def getspecific(self, index, mask):
+        # load image
+        x = Image.open(f'./data/cars/Audi_processed_24px/{index+1}.jpg')
+        x = self.transform(x)
+        x = torch.reshape(x, (1, 24*24))
+        # x = torch.flatten(x)
+        
+        to_predict = [mask]
+
+        target1 = x[0][to_predict[0]].clone().unsqueeze(0)
+
+        pixel_value = torch.tensor([target1 * 255.0]).to(torch.long)
+        pixel_value = pixel_value.unsqueeze(0)
+        #pixel_value = torch.nn.functional.one_hot(pixel_value, num_classes=256).squeeze(0).float()
+
+
+        x[0][to_predict[0]:] = 0.0
+        #x[0][to_predict[0]] = -1.0
+        
+        x = torch.flatten(x)
+
+        # position_to_predict = to_predict[0] / 256.0
+
+        # # print(x, x.shape)
+        # # print(position_to_predict, position_to_predict.shape)
+
+        # x = torch.cat((position_to_predict.unsqueeze(0), x))
+
+        return x, pixel_value
+
+    def __getitem__(self, index):
+        # load image
+        x = Image.open(f'./data/cars/Audi_processed_24px/{index+1}.jpg')
+        x = self.transform(x)
+        x = torch.reshape(x, (1, 24*24))
+        # x = torch.flatten(x)
+        
+        to_predict = torch.randint(0, 24*24-1, (1,))
+
+        target1 = x[0][to_predict[0]].clone().unsqueeze(0)
+
+        pixel_value = torch.tensor([target1 * 255.0]).to(torch.long)
+        pixel_value = pixel_value.unsqueeze(0)
+        #pixel_value = torch.nn.functional.one_hot(pixel_value, num_classes=256).squeeze(0).float()
+
+
+        x[0][to_predict[0]:] = 0.0
+        #x[0][to_predict[0]] = -1.0
+        
+        x = torch.flatten(x)
+
+        # position_to_predict = to_predict[0] / 256.0
+
+        # # print(x, x.shape)
+        # # print(position_to_predict, position_to_predict.shape)
+
+        # x = torch.cat((position_to_predict.unsqueeze(0), x))
+
+        return x, pixel_value
+
+class AudiAutoregressionColor24px(Dataset):
     def __init__(self):
         # grayscale transform
         self.transform = transforms.Compose([transforms.ToTensor()])
 
     def __len__(self):
         return 5190
+
+    
 
     def __getitem__(self, index):
         # load image
